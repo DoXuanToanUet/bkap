6c1b335b6922b8be8719e5341cfb7234e4a57273
toan <toan@gmail.com>
2022-10-22 11:45:37 +0700

    update menu

--- js/main.js
+++ js/main.js
@@ -11,96 +11,108 @@
                 $(".header").removeClass('fix-header');
             }   
         });
+
+        $(window).scroll(function() {
+          var height = $(window).scrollTop();
+
+          if (height > 100) {
+              $(".scrolltoTop").fadeIn();
+          } else {
+              $(".scrolltoTop").fadeOut();
+          }   
+        });
+        $('.header-bottom-ipad li').click(function(){
+          $(this).find('.sub-menu').slideToggle();
+        })
+        $('.humberger').click(function(){
+          // $(this).
+          $(this).closest('.header-right').find('.header-bottom-ipad  ').slideToggle();
+        })
     })
-  
-
-    var bannerSwiper = new Swiper('.home-banner-swiper', {
-        spaceBetween: 20,
-        slidesPerView: 1,
-        // autoplay: {
-        //     delay: 5000,
-        // },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-       
-       
-    });
-    var investSwiper = new Swiper('.invest-field-swiper', {
-        spaceBetween: 20,
-        slidesPerView: 4,
-        // autoplay: {
-        //     delay: 5000,
-        // },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-       
-       
-    });
-    
-    new Swiper('.news-project-swiper', {
-            slidesPerView: 2,
-            // slidesPerView: "auto",
-
-            spaceBetween: 20,
-
-            pagination: {
-              el: '#project-tab-1 .pagination-page',
-              clickable: true,
-            },
-            breakpoints: {
-
-              320: {
-                slidesPerView: 1,
-                slidesPerColumn: 1,
-              },
-              480: {
-                slidesPerView: 1,
-                slidesPerColumn: 1,
-              },
-              768: {
-                slidesPerView: 1,
-                slidesPerColumn: 1,
-              },
-            }
-    });
-    
-    new Swiper('.news-thumbnail-swiper', {
-        slidesPerView: 4,
-        // slidesPerView: "auto",
-
-        spaceBetween: 20,
-
-        pagination: {
-          el: '#project-tab-1 .pagination-page',
-          clickable: true,
-        },
-        breakpoints: {
-
-          320: {
-            slidesPerView: 1,
-            slidesPerColumn: 1,
-          },
-          480: {
-            slidesPerView: 1,
-            slidesPerColumn: 1,
-          },
-          768: {
-            slidesPerView: 1,
-            slidesPerColumn: 1,
-          },
-        }
-    });
-
    
-    
 })(jQuery);
 
+new Swiper('.home-banner-swiper', {
+  spaceBetween: 20,
+  slidesPerView: 1,
+  // autoplay: {
+  //     delay: 5000,
+  // },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+ 
+ 
+});
+ new Swiper('.invest-field-swiper', {
+  spaceBetween: 20,
+  slidesPerView: 4,
+  // autoplay: {
+  //     delay: 5000,
+  // },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+ 
+ 
+});
 
-var newsProjectSlider = new Swiper('.news-project-swiper', {
+new Swiper('.news-project-swiper', {
+      slidesPerView: 2,
+      // slidesPerView: "auto",
+
+      spaceBetween: 20,
+
+      pagination: {
+        el: '#project-tab-1 .pagination-page',
+        clickable: true,
+      },
+      breakpoints: {
+
+        320: {
+          slidesPerView: 1,
+          slidesPerColumn: 1,
+        },
+        480: {
+          slidesPerView: 1,
+          slidesPerColumn: 1,
+        },
+        768: {
+          slidesPerView: 1,
+          slidesPerColumn: 1,
+        },
+      }
+});
+
+new Swiper('.news-thumbnail-swiper', {
+  slidesPerView: 4,
+  // slidesPerView: "auto",
+
+  spaceBetween: 20,
+
+  pagination: {
+    el: '#project-tab-1 .pagination-page',
+    clickable: true,
+  },
+  breakpoints: {
+
+    320: {
+      slidesPerView: 1,
+      slidesPerColumn: 1,
+    },
+    480: {
+      slidesPerView: 1,
+      slidesPerColumn: 1,
+    },
+    768: {
+      slidesPerView: 1,
+      slidesPerColumn: 1,
+    },
+  }
+});
+ new Swiper('.news-project-swiper', {
   slidesPerView: 1,
   // slidesPerView: "auto",
   
